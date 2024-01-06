13f0f14feaf73c2b111b44142550c9ac44d1bd24
Hiba Anwar <hiba2anwar@gmail.com>
2024-01-06 19:08:21 +0400

    feat(search): Add search results functionality
    Set up API data fetching and displaying on UI

--- src/components/SearchPanel.jsx
+++ src/components/SearchPanel.jsx
@@ -1,8 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./SearchPanel.module.css";
+
+const url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+const options = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "9219e702eamsh20426df85e13c8cp169243jsnf797d4cc9c8d",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
 
 function SearchPanel() {
 	const [searchQuery, setSearchQuery] = useState("");
+	const [searchResults, setSearchResults] = useState([]);
+
+	useEffect(() => {
+		if (!searchQuery) return;
+
+		async function fetchSearchResults() {
+			try {
+				const res = await fetch(
+					`${url}/cities?types=CITY&minPopulation=10000&namePrefix=${searchQuery}`,
+					options
+				);
+				const result = await res.json();
+				setSearchResults(result.data);
+			} catch (error) {
+				throw new Error(error);
+			}
+		}
+		fetchSearchResults();
+	}, [searchQuery]);
 
 	return (
 		<div>
@@ -19,7 +47,15 @@ function SearchPanel() {
 				className={`${styles.searchResults} ${
 					searchQuery ? styles.active : ""
 				}`}>
-				<ul></ul>
+				{searchResults && (
+					<ul>
+						{searchResults.map((results) => (
+							<li key={results.wikiDataId}>
+								{results.city}, {results.countryCode}
+							</li>
+						))}
+					</ul>
+				)}
 			</div>
 		</div>
 	);
